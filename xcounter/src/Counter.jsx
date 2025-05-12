6eec6ed96ed9cc20e8dca823927a9f74d9295d72
Sucharita Lahiri <sucharita14.lahiri@gmail.com>
2025-05-12 13:12:45 +0530

    Added counter component

---
+++ xcounter/src/Counter.jsx
@@ -0,0 +1,24 @@
+import  { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1><b>Counter App</b></h1>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
